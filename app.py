9293d2f9dbf9d5ec221716180368d4ad5b5724de
pgk95 <kalupgk@gmail.com>
2023-09-13 12:41:13 +0300

    added some comments for the codes

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# import neccessary modules
 from flask import render_template, request, redirect, url_for, flash, session, Flask
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
@@ -7,35 +8,41 @@ from .models import User, db
 import os
 import smtplib
 
-
+# Load environment variables from .env file
 load_dotenv()
+
+# Initialize Flask application
 app = Flask(__name__)
 
+# Configure the Flask app with database URI and secret key from environment variables
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_CRIDENTIALS')
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
+# Intialize SQLAlchemy
 db.init_app(app)
 
-
+# Initialize Bcrypt for password hashing
 bcrypt = Bcrypt()
 
+# Definethe route for the homepage 
 @app.route('/', strict_slashes=False)
 def index():
     username = session.get('username')
     return render_template("index.html", username=username)
 
 
+# Define the route for the course page
 @app.route('/course', strict_slashes=False)
 def course():
     return render_template('courses.html')
 
-
+# Define the route for the about page
 @app.route('/about', strict_slashes=False)
 def about():
     return render_template('about.html')
 
 
-
+# Define the route for user registration (signup)
 @app.route('/signup', methods=['GET', 'POST'], strict_slashes=False)
 def signup():
     if request.method == 'POST':
@@ -43,10 +50,12 @@ def signup():
         email = request.form.get('email')
         fullname = request.form.get('fullname')
         password = request.form.get('password_input')
+
+        # Hash the user's password before storing it
         hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
 
+        # Create a new User object and add it to the database
         new_user = User(username=username, email=email, fullname=fullname, password_hash=hashed_password)
-
         db.session.add(new_user)
         db.session.commit()
 
@@ -56,15 +65,18 @@ def signup():
     return render_template('signup.html')
 
 
+# Define the route for user login
 @app.route('/login', methods=['GET', 'POST'],)
 def login():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
 
+        # Query the database to check if the user exists and the password is correct
         user = User.query.filter_by(username=username).first()
 
         if user and bcrypt.check_password_hash(user.password_hash, password):
+            # Set a session variable to track the user's login status
             session['username'] = username
             flash(f'Welcome, {username}!', 'success')
             return redirect(url_for('index'))
@@ -73,8 +85,10 @@ def login():
     return render_template('login.html') 
 
 
+# Define the route for logout
 @app.route('/logout')
 def logout():
+    # Clear the user's session data to log them out
     session.clear()
     flash('you have been logged out','success')
     return redirect(url_for('index'))
@@ -94,6 +108,6 @@ def contact():
         return render_template('contact.html')
 
 
-
+# Run the flask application ifthis script is executed directly
 if __name__ == '__main__':
     app.run(debug=True)
--- models.py
+++ models.py
@@ -1,12 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
 
+# Initialize SQLAlchemy
 db = SQLAlchemy()
 
+# Define a User model
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(60), nullable=False)
-    fullname = db.Column(db.String(100), nullable=False)
+    id = db.Column(db.Integer, primary_key=True) # Define columuns for the User table
+    username = db.Column(db.String(20), unique=True, nullable=False) # Username column
+    email = db.Column(db.String(120), unique=True, nullable=False) # Email column
+    password_hash = db.Column(db.String(60), nullable=False) # Password hash column
+    fullname = db.Column(db.String(100), nullable=False) # Full name column
 
+    # Set the table name to 'users'
     __tablename__ = 'users'
